13cf5f36affbede0c54fb3edef7063c0546b68b9
Marcel Gerber <mgerber59@gmail.com>
2020-09-26 21:54:34 +0200

    fix: only export browser-ready method

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-'use strict';
-const filenamify = require('./filenamify');
-const filenamifyPath = require('./filenamify-path');
-
-const filenamifyCombined = filenamify;
-filenamifyCombined.path = filenamifyPath;
+"use strict";
+const filenamify = require("./filenamify");
 
 module.exports = filenamify;
